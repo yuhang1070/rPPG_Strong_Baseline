230146eefea91a7be5bafe5f0aa781827ba907ee
YuHang <37216286+yuhang1070@users.noreply.github.com>
2022-06-13 15:11:00 +0800

    Update eval_tools.py

--- tools/eval_tools.py
+++ tools/eval_tools.py
@@ -23,6 +23,28 @@ def compute_hr_metric(pred, label):
 
     return sd, mae, rmse, r
 
+def cal_hr_metric(hr_gt, hr_es, ):
+    hr_gt = np.array(hr_gt, dtype=np.float32)
+    hr_es = np.array(hr_es, dtype=np.float32)
+    hr_err = hr_gt - hr_es
+
+    STD = np.std(hr_err)
+    MAE = np.mean(np.abs(hr_err))
+    RMSE = np.sqrt(np.mean(np.square(hr_err)))
+    R, _ = pearsonr(hr_es, hr_gt)
+
+    ME = np.mean(hr_err)
+    MER = np.mean(np.abs(hr_err) / hr_gt)
+
+    return {
+        'STD': float(STD),
+        'MAE': float(MAE),
+        'RMSE': float(RMSE),
+        'R': float(R),
+        'ME': float(ME),
+        'MER': float(MER),
+    }
+
 
 if __name__ == '__main__':
     x_gt = [1, 2, 3, 4]
